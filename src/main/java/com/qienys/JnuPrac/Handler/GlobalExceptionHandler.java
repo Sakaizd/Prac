3f7f7d3d26e5c386734b3dbc8132a885bde9c96a
Qienys <819355396@qq.com>
2019-07-19 17:54:51 +0800

    upload

--- src/main/java/com/qienys/JnuPrac/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/qienys/JnuPrac/Handler/GlobalExceptionHandler.java
@@ -8,8 +8,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    //https://jira.spring.io/browse/SPR-14651
-    //4.3.5 supports RedirectAttributes redirectAttributes
+
     @ExceptionHandler(MultipartException.class)
     public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
