04a17939b472e021b5bceccc39bc78f25eb77c12
Qienys <819355396@qq.com>
2019-07-19 14:03:50 +0800

    sql

--- src/main/resources/sql/test.sql
+++ src/main/resources/sql/test.sql
@@ -1,133 +1,199 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 80016
-Source Host           : localhost:3306
-Source Database       : test
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3306
+ Source Schema         : test
 
-Target Server Type    : MYSQL
-Target Server Version : 80016
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
 
-Date: 2019-07-15 23:19:01
+ Date: 19/07/2019 14:03:19
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for cart
 -- ----------------------------
 DROP TABLE IF EXISTS `cart`;
-CREATE TABLE `cart` (
+CREATE TABLE `cart`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `number` bigint(20) DEFAULT NULL,
-  `product_id` bigint(20) DEFAULT NULL,
-  `uid` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `active` bit(1) NOT NULL,
+  `brand_id` bigint(20) NULL DEFAULT NULL,
+  `count` bigint(20) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `price` double NULL DEFAULT NULL,
+  `product_id` bigint(20) NULL DEFAULT NULL,
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type_id` bigint(20) NULL DEFAULT NULL,
+  `uid` bigint(20) NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of cart
 -- ----------------------------
+INSERT INTO `cart` VALUES (9, b'1', 1, 2, '一只手机', 3500, 1, 'p30', 1, 1, NULL);
+INSERT INTO `cart` VALUES (8, b'1', 1, 3, '一只手机', 4000, 2, 'p30pro', 1, 1, NULL);
+INSERT INTO `cart` VALUES (6, b'1', 3, 3, '电脑', 5000, 4, '暗影精灵', 2, 1, NULL);
+INSERT INTO `cart` VALUES (7, b'1', 2, 3, '一只手机', 6000, 3, 'mi9', 1, 1, NULL);
 
 -- ----------------------------
--- Table structure for orders
+-- Table structure for hibernate_sequence
 -- ----------------------------
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `address` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `pay_status` bit(1) NOT NULL,
-  `post_status` bit(1) NOT NULL,
-  `telephone` varchar(255) DEFAULT NULL,
-  `total_price` double DEFAULT NULL,
-  `uid` bigint(20) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence`  (
+  `next_val` bigint(20) NULL DEFAULT NULL
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
--- Records of orders
+-- Records of hibernate_sequence
 -- ----------------------------
+INSERT INTO `hibernate_sequence` VALUES (1);
+INSERT INTO `hibernate_sequence` VALUES (1);
 
 -- ----------------------------
 -- Table structure for order_products
 -- ----------------------------
 DROP TABLE IF EXISTS `order_products`;
-CREATE TABLE `order_products` (
+CREATE TABLE `order_products`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `number` bigint(20) DEFAULT NULL,
-  `order_id` bigint(20) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `product_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `count` bigint(20) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `order_id` bigint(20) NULL DEFAULT NULL,
+  `price` double NULL DEFAULT NULL,
+  `product_id` bigint(20) NULL DEFAULT NULL,
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order_products
 -- ----------------------------
+INSERT INTO `order_products` VALUES (24, 1, '一只手机', 1152050752823230464, 6000, 3, 'mi9');
+INSERT INTO `order_products` VALUES (23, 1, '电脑', 1152050752823230464, 5000, 4, '暗影精灵');
+INSERT INTO `order_products` VALUES (22, 1, '一只手机', 1152050752823230464, 4000, 2, 'p30pro');
+INSERT INTO `order_products` VALUES (21, 1, '一只手机', 1152050752823230464, 3500, 1, 'p30');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `order_id` bigint(20) NULL DEFAULT NULL,
+  `pay_status` bit(1) NOT NULL,
+  `post_status` bit(1) NOT NULL,
+  `telephone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `total_price` double NULL DEFAULT NULL,
+  `uid` bigint(20) NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+INSERT INTO `orders` VALUES (7, 'JNU', 'wyc', 1152050752823230464, b'0', b'0', '120', 18500, 1, '2019-07-19 03:01:18');
 
 -- ----------------------------
 -- Table structure for product
 -- ----------------------------
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product` (
+CREATE TABLE `product`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `active` bit(1) NOT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `product_type` varchar(255) DEFAULT NULL,
-  `sold` varchar(255) DEFAULT NULL,
-  `stock` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `brand_id` bigint(20) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `price` double NULL DEFAULT NULL,
+  `sold` bigint(20) NULL DEFAULT NULL,
+  `stock` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type_id` bigint(20) NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
+INSERT INTO `product` VALUES (1, b'1', 1, '一只手机', 'p30', 3500, 0, '120', 1, NULL);
+INSERT INTO `product` VALUES (2, b'1', 1, '一只手机', 'p30pro', 4000, 0, '150', 1, NULL);
+INSERT INTO `product` VALUES (3, b'1', 2, '一只手机', 'mi9', 6000, 1, '300', 1, NULL);
+INSERT INTO `product` VALUES (4, b'1', 3, '电脑', '暗影精灵', 5000, 0, '100', 2, NULL);
+
+-- ----------------------------
+-- Table structure for product_properties
+-- ----------------------------
+DROP TABLE IF EXISTS `product_properties`;
+CREATE TABLE `product_properties`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `brand_id` bigint(20) NULL DEFAULT NULL,
+  `brand_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type_id` bigint(20) NULL DEFAULT NULL,
+  `type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of product_properties
+-- ----------------------------
+INSERT INTO `product_properties` VALUES (1, 1, 'huawei', 1, '手机');
+INSERT INTO `product_properties` VALUES (2, 2, 'xiaomi', 1, '手机');
+INSERT INTO `product_properties` VALUES (3, 1, 'huawei', 2, '电脑');
+INSERT INTO `product_properties` VALUES (4, 3, 'HP', 2, '电脑');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `create_time` datetime DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_type` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `user_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '2019-07-15 22:44:32', 'c3079d243353e68687ea9b6794036998', '1', 'admin', 'admin');
-INSERT INTO `user` VALUES ('2', '2019-07-15 22:44:49', '7ff525e7475c375a416652f88b11fb7d', '1', 'user', 'user');
+INSERT INTO `user` VALUES (2, '2019-07-15 22:44:32', 'c3079d243353e68687ea9b6794036998', '1', 'admin', 'admin');
+INSERT INTO `user` VALUES (1, '2019-07-15 22:44:49', '03ffd09b3fa907b291aa35610c38c858', '1', 'user', 'user');
+INSERT INTO `user` VALUES (6, '2019-07-18 12:00:02', '31919a252cb9261fcac165f15c04e889', '1', '123', 'user');
 
 -- ----------------------------
 -- Table structure for user_info
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info` (
+CREATE TABLE `user_info`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `address` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `id_card` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `psw_ans` varchar(255) DEFAULT NULL,
-  `psw_ques` varchar(255) DEFAULT NULL,
-  `telephone` varchar(255) DEFAULT NULL,
-  `uid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `id_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `psw_ans` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `psw_ques` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `telephone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `uid` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `uid`(`uid`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
+INSERT INTO `user_info` VALUES (1, 'jnu', 'i@qq.com', '404', 'wyc', 'wyc', 'who am i', '110', 2);
+INSERT INTO `user_info` VALUES (2, 'jnu', 's@qq.com', '405', 'sbj', 'sbj', 'who am i', '120', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
